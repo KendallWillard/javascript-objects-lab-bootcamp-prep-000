836fb6ecefa248977a531250b8a51d2f32eedfd2
kendall willard <kendallwillard@hotmail.com>
2019-01-27 22:36:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,5 +10,8 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
 }
 
 function deleteFromObjectByKey(obj, key) {
+  var newObj = Object.assign({}, obj) 
   
+  delete newObj.key
+  return newObj
 }
